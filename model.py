e83c1a50134099768fb54a002eac745e028c136d
spelaplevel <spela.plevel@gmail.com>
2020-07-18 01:11:14 +0200

    Popravljena funkcija 'postavi' v modelu

--- model.py
+++ model.py
@@ -1,19 +1,26 @@
 class Igra(): 
+    '''
+    Polje : [polje[0],polje[1]],
+     polje[i]: igralna plošča itega igralca
+     polje[i][vrstica][stolpec] == -1: na mestu [vrstica][stolpec] ni nobene ladice
+     polje[i][v][s] == n: namestu v,s je ladica z indeksom n (to je indeks v tabeli ladice)
+
+     [2,4,4,5]
+    '''
     def __init__(self, st_nepotopljenih, ladjice): 
-        self.polje = [[[0] * 10] * 10, [[0] * 10] * 10]
+        self.polje = [[[-1] * 10 for i in range(10)], [[-1] * 10 for i in range(10)]]]
         self.zgodovina_strelov = [[[0] * 10] * 10, [[0] * 10] * 10]
         self.st_nepotopljenih = [st_nepotopljenih] * 2
         self.ladjice = [ladjice] * 2
 
-    def postavi(self, postavitev, vrstica, stolpec, dolzina): 
-        if vrstica, stolpec in range(1, 11):
+    def postavi(self, postavitev, vrstica, stolpec, indeks, na_vrsti): 
+        if vrstica in range(0, 10) and stolpec in range(0,10):
             # Ali je postavitev pravilna, če je ladjica vodoravna.
-            if postavitev == 0 and int(self.polje[na_vrsti][vrstica][stolpec + dolzina]) in range(1, 11):
-                self.polje[na_vrsti][vrstica][stolpec:stolpec + dolzina] = dolzina #nastavimo vsa mesta kjer je ladjica na številko ladjice
-            elif postavitev == 1 and int(self.polje[na_vrsti][vrstica + dolzina][stolpec]) in range(1, 11):
-                self.polje[na_vrsti][vrstica:vrstica + dolzina][stolpec] = dolzina
-        else: 
-            return 'napačno postavljena'
+            if postavitev == 0 and stolpec + self.ladjice[na_vrsti][indeks] <= 10: 
+                for i in range(stolpec, stolpec + self.ladjice[na_vrsti][indeks]): 
+                    self.polje[na_vrsti][vrstica][i] = indeks
+                return True
+        return False
 
     def poteza(self, na_vrsti, vrstica, stolpec): 
         if polje[na_vrsti][vrstica][stolpec] > -1:
