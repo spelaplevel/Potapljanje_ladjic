8bfc7b0d5f1b6dc172afe5df62a2c3d5f1b3a2c0
spelaplevel <spela.plevel@gmail.com>
2020-09-04 01:47:28 +0200

    Slovnični popravki

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -115,25 +115,25 @@ def offline_post():
         if igra.igralec_na_vrsti in range(0, 2):
             bottle.redirect('/postavljanje_offline/')
         else:
-            bottle.redirect('/navodila/') #potem premaknem na kaj drugega, zaenkrat tko samo tok da vidm, drugo polje!!!
+            bottle.redirect('/navodila/')
         
 
-    #igra.index += 1 oziroma ga daš na 0, če je velčji od dolžine tabele
-    #če g daš na 0, greš nanaslednjega igralca
-    #če si na zadnjem igralcu, se premakneš na /igra_offline/ al pa kej potobnega
+
 @bottle.get('/igra_offline/')
 def igra_get():
     igra = pridobi_igro()[0]
-    return bottle.template('igra.html', get_url=bottle.url, igra=igra, na_vrsti=igra.igralec_na_vrsti, tekst='' )
+    return bottle.template('igra.html', get_url=bottle.url, igra=igra, na_vrsti=igra.igralec_na_vrsti, tekst='')
+    
 @bottle.get('/igra_offline/<tekst>')
 def igra_get(tekst):
     igra = pridobi_igro()[0]
-    return bottle.template('igra.html', get_url=bottle.url, igra=igra, na_vrsti=igra.igralec_na_vrsti, tekst=str(tekst) )
+    return bottle.template('igra.html', get_url=bottle.url, igra=igra, na_vrsti=igra.igralec_na_vrsti, tekst=str(tekst))
+    
 @bottle.post('/igraj_offline/')
 def igraj_offline():
     igra = pridobi_igro()[0]
     ime_gumba = list(bottle.request.forms.keys())[0]
-    if igra.indeks_trenutne_ladjice == -1: # smo že naredili potezo in zdj čakamo, da tipo pritisne gumb naslednji igralec
+    if igra.indeks_trenutne_ladjice == -1: # smo že naredili potezo in sedaj čakamo, da uporabnik pritisne gumb naslednji igralec
         if ime_gumba == 'naslednji_igralec':
             igra.indeks_trenutne_ladjice = 0
             igra.igralec_na_vrsti = not igra.igralec_na_vrsti
@@ -145,20 +145,17 @@ def igraj_offline():
         bottle.redirect('/domov/')
     vrstica = int(ime_gumba.split('_')[0])
     stolpec = int(ime_gumba.split('_')[1])
-    # nuklearka mejbi opcija ?
     poteza = igra.poteza(not igra.igralec_na_vrsti, vrstica, stolpec)
     if poteza == 'zmaga':
-        print('reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
         igra.indeks_trenutne_ladjice = -2
     else:
         igra.indeks_trenutne_ladjice = -1
     bottle.redirect(f'/igra_offline/{poteza}')
 
-    
-
     bottle.redirect('/igra_offline/')
+
 @bottle.get('/nastavitve/<n>')
-def nastaviktve(n):
+def nastavitve(n):
         igra = pridobi_igro()[0]
         return bottle.template('nastavitve.html', get_url=bottle.url, korak=n, igra=igra)
 
@@ -185,7 +182,6 @@ def zgodovina_post():
     ime_gumba = list(bottle.request.forms.keys())[0]
     if ime_gumba == 'offline':  # Pritisnil je prvi gumb. 
         igra = pridobi_igro()[0]
-        # print(f'igra: {type(igra)}')
         igra.__init__(igra.standardne_ladje) # Začnemo NOVO igro. 
         bottle.redirect('/postavljanje_offline/')
     if ime_gumba == 'nastavitve':
@@ -196,20 +192,3 @@ def zgodovina_post():
         #online
     
 bottle.run(debug=True, reloader=True, host='localhost')
-
-'''
-TODO : 
-
-potem ko en igralec postavi svoje ladjice, pokaži novo polje še za drugega
-
-self.trenutni index ladice in self. na vrsti je  daš v Igra(), in potem model.Igra() vednio ve , kdo je na vrsti in katero ladijco postavljamo
-
-nato funkcija postavi offline
-
-
-
-na koncu naj se izpiše zmagovalec
-
-lep html
-
-'''
